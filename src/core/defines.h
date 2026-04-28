70cf1e05db71fb83a7381a5ba68f6cc4fdafb890
MarkusNuuja <xyntexx@users.noreply.github.com>
2026-04-28 15:10:30 +0300

    Bump firmware version to 0.42.1
    Patch release containing fixes for RTCM buffer overflow and UART
    buffering issues introduced in 0.42.0.

--- src/core/defines.h
+++ src/core/defines.h
@@ -34,7 +34,7 @@
 #define STATIC_SN 255, 255, 255, 0
 #define STATIC_DNS 8, 8, 8, 8
 
-#define FIRMWARE_VERSION "0.42.0"  // Update this when you release new versions
+#define FIRMWARE_VERSION "0.42.1"  // Update this when you release new versions
 #define BUILD_DATE __DATE__ " " __TIME__
 
 // Task Priorities
